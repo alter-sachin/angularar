eb45afa22709a313f41ecf73057cfe943b6230be
Raman Guleria <rguleria@expedia.com>
2017-11-17 09:20:31 +0530

    Loading AR Models from menu

--- src/app/restaurantdetail/restaurantdetail.component.ts
+++ src/app/restaurantdetail/restaurantdetail.component.ts
@@ -15,6 +15,8 @@ export class RestaurantDetailComponent implements OnInit {
   @Output() close = new EventEmitter();
   error: any;
   navigated = false; // true if navigated here
+  activeDish: Dish;
+  modelUrl = 'https://s3.ap-south-1.amazonaws.com/proghackersarmodels/restaurant_sample_donut_v1/scene.gltf';
 
   constructor(private restaurantService: RestaurantService,
               private route: ActivatedRoute,
@@ -23,11 +25,18 @@ export class RestaurantDetailComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.forEach((params: Params) => {
+      console.log('I am here' + 'params[id]' + params['id']);
       if (params['id'] !== undefined) {
-        const id = +params['id'];
+        const id = params['id'];
         this.navigated = true;
         this.restaurantService.getRestaurant(id)
-          .then(restaurant => this.restaurant = restaurant);
+          .then(restaurant => {
+              this.restaurant = restaurant;
+              console.log(restaurant);
+              this.activeDish = this.restaurant.menu[0];
+              console.log(this.activeDish);
+            }
+          );
       } else {
         this.navigated = false;
         this.restaurant = new Restaurant();
@@ -44,6 +53,13 @@ export class RestaurantDetailComponent implements OnInit {
     if (this.navigated) {
       window.history.back();
     }
+  }
+
+  loadDish(dish: Dish): void {
+    this.activeDish = dish;
+    const sceneAl = document.querySelector('a-scene');
+    const entityAl = document.querySelector('a-entity');
+    entityAl.setAttribute('gltf-model-next', 'src: url(' + dish.modelLocation + ');');
   }
 
 }
