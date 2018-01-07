4cb2e2427a7611467d88d391beb5572eeddb07aa
Lofty Khanna <loftykhanna@gmail.com>
2018-01-08 01:18:11 +0530

    fixed menu toggle issue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,28 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit , AfterViewInit{
+iframe2Url:any ="https://sketchfab.com/models/5ef4a0ebf5bd4008976f8ef88c306b3e/embed?autostart=1";
+  iframe1Url:any="https://sketchfab.com/models/acf17ee90c0043bd89fa4b635883f665/embed" ;
 
-  constructor() {
+  constructor(private element:ElementRef) {
   }
+
 
   ngOnInit() {
   }
 
+  ngAfterViewInit()
+  {
+  document.getElementById("iframe1").setAttribute("src",this.iframe1Url);
+    document.getElementById("iframe2").setAttribute("src",this.iframe2Url);
+  }
+
+  toggleMenu():void{
+    this.element.nativeElement.querySelector('#MenuOverlay').classList.toggle('open')
+  }
 }
